ed10ce66c6d131bd5eef3321e1cae6176fe4e46f
kayware <kay@kayone.pro>
2025-09-16 15:39:06 +0200

    More logging

--- components/comfoair/comfoair.h
+++ components/comfoair/comfoair.h
@@ -305,7 +305,7 @@ protected:
       // checksum is without checksum bytes
       uint8_t checksum = comfoair_checksum_(data_ + 2, COMMAND_LEN_HEAD + data_length - 2);
       if (checksum != byte) {
-        ESP_LOGW(TAG, "%02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X", data_[0], data_[1], data_[2], data_[3], data_[4], data_[5], data_[6], data_[7], data_[8], data_[9], data_[10]);
+        ESP_LOGW(TAG, "%02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X", data_[0], data_[1], data_[2], data_[3], data_[4], data_[5], data_[6], data_[7], data_[8], data_[9], data_[10], data_[11], data_[12], data_[13], data_[14], data_[15], data_[16]);
         ESP_LOGW(TAG, "ComfoAir Checksum doesn't match: 0x%02X!=0x%02X", byte, checksum);
         for (uint16_t i = 0; i < data_length; i++)
         return false;
