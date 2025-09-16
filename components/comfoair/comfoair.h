eaad35291b5aff2f9333a75f2bf017196367114a
kayware <kay@kayone.pro>
2025-09-16 15:32:27 +0200

    Increase freq

--- components/comfoair/comfoair.h
+++ components/comfoair/comfoair.h
@@ -20,7 +20,7 @@ public:
   // Poll every 600ms
   ComfoAirComponent() :
   Climate(),
-  PollingComponent(10000),
+  PollingComponent(1000),
   UARTDevice() { }
 
   /// Return the traits of this controller.
